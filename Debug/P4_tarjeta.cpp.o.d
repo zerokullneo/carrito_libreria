e65ba9517de6fd80e0971f8f5346f5eb244fe170
Jose M Barba Gonzalez <zerokullneo@localhost.localdomain>
2016-02-02 14:17:32 +0100

    Actualizando Makefile
    Cambios en en Makefile para compilar todo.

--- Debug/P4_tarjeta.cpp.o.d
+++ Debug/P4_tarjeta.cpp.o.d
@@ -2,7 +2,7 @@ Debug/P4_tarjeta.cpp.o: P4/tarjeta.cpp P4/tarjeta.h P4/../cabeceras.h \
  P4/usuario.h P4/../P1/Cadena/cadena.h P4/../P1/Cadena/../../cabeceras.h \
  P4/../P1/Fecha/fecha.h P4/../P1/Fecha/../../cabeceras.h \
  P4/../P1/Fecha/../Cadena/cadena.h \
- P4/../P1/Fecha/../Cadena/../../cabeceras.h P4/articulo.h autor.h
+ P4/../P1/Fecha/../Cadena/../../cabeceras.h P4/articulo.h
 
 P4/tarjeta.h:
 
@@ -23,5 +23,3 @@ P4/../P1/Fecha/../Cadena/cadena.h:
 P4/../P1/Fecha/../Cadena/../../cabeceras.h:
 
 P4/articulo.h:
-
-autor.h:
